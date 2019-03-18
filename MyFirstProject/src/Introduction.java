aaa730278b2fde81a37a36511839bd7aa14541b5
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2019-03-18 00:41:13 -0700

    Add files via upload

---
+++ MyFirstProject/src/Introduction.java
@@ -0,0 +1,10 @@
+//Meghann Benson this is my work. 
+public class Introduction {
+	public static void main(String[] args) {
+
+		System.out.println("My name is Meg.");
+		System.out.println("My favorite color is blue.");
+		System.out.println("If I won the lottery, I would travel.");
+
+	}
+}
